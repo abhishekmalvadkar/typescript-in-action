06ca501ad59fa87f3d1502e796ce11ac2f228dfb
Abhishek <abhi@dev.com>
2025-10-05 20:55:52 +0530

    Create sample test with github action configuration

---
+++ src/app/calculator.spec.ts
@@ -0,0 +1,10 @@
+describe('Calculator', () => {
+
+  it('should perform sum of two numbers', () => {
+    const number1 = 5;
+    const number2 = 6;
+    const result = number1 + number2;
+    expect(result).toBe(11);
+  });
+
+});
